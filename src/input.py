d5169d126f59e1ce4f188ff9673423a729fbcdf9
mpotts <mpotts1@umbc.edu>
2020-03-28 15:04:05 -0400

    Sanitize input for add_class

--- src/input.py
+++ src/input.py
@@ -2,9 +2,6 @@
 
 #Basic class functions
 
-
-
-
 '''Data needed -
     CLASS
 	c_instructor	varchar(30),
@@ -21,13 +18,44 @@
 
 def add_class(instructor, students, duration, time, dept, number, section, day_of_week):
 
+    input = {"instructor": instructor, "students": students, "duration": duration, "time": time, "dept": dept, "number": number, "section": section, "day_of_week": day_of_week}
+    data_types = {
+        "instructor" : str,
+        "students" : int,
+        "duration" : int,
+        "time" : str,
+        "dept" : str,
+        "number" : str,
+        "section": int,
+        "day_of_week" : list
+    }
+
+    invalid_data = []
+    for key in data_types.keys():
+        if not(isinstance(input[key], data_types[key])):
+            print(data_types[key], type(input[key]))
+            invalid_data.append(key)
+
+    if(len(invalid_data) > 0):
+        return invalid_data
+
+    #Call some other function here to generate constraints,
 
 
 
 ''' need primary key(c_dept, c_number, c_section) '''
 def remove_class(dept, number, section):
-
+    print("Remove Class")
 
 
 def modify_class():
+    print("Modify Class")
 
+
+
+#Test Function input
+#Good
+#add_class("Test Instructor", 3, 90, "12:30","CMSC", "447", 0, ["Monday", "Wednesday"])
+
+#Bad
+#add_class("Test Instructor", "3", "90", 1230, "CMSC", 447, "0", "[Monday,Wednesday]")
