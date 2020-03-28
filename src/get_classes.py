eb5eaf6f7896d14c8bd9157e05e08837c0e7e43a
crandallowen <crow4@umbc.edu>
2020-03-28 15:45:48 -0400

    Created functionsToAdd Created get_classes

--- src/get_classes.py
+++ src/get_classes.py
@@ -1,4 +1,18 @@
 import constraint
 
+def time_room_constraint(room, time, duration, day_of_week):
+    if room.isEmpty(time, duration, day_of_week):
+        return True
+
 def get_available_class(instructor, students, duration, time, dept, number, section, day_of_week):
     problem = constraint.Problem()
+
+    problem.addVariable("room", [set of rooms]) #"Set of rooms" will be replaced by the column of rooms
+
+    problem.addConstraint(time_room_constraint, ["time", "room", "duration", "day_of_week"])
+
+    solutions = problem.getSolutions()
+    return solutions
+
+
+    
