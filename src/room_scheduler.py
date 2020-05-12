44a8a2063c129d48d86aaf0418ee42d96d3ed9db
crandallowen <crow4@umbc.edu>
2020-05-12 11:32:48 -0400

    Debugging on scheduler

--- src/room_scheduler.py
+++ src/room_scheduler.py
@@ -3,7 +3,6 @@ import src.sql_handler
 import datetime
 
 room_dict = {}
-class_dict = {}
 
 #A constraint function to check if the room is a sufficient size
 def room_size_cons(*class_args):
@@ -69,8 +68,8 @@ def get_classID(_class):
     return str(_class['dept']) + str(_class['number']) + ':' + str(_class['section'])
 
 def print_solution(solution):
-    for classID in class_dict.keys():
-        print("{} : {}".format(classID, get_roomID(solution['classID'])))
+    for classID in solution:
+        print("{} : {}".format(classID, get_roomID(solution[classID])))
 
 #This function takes in a list of room and class dicts and returns all of the configurations
 # of classes and rooms within the constraints
@@ -78,10 +77,10 @@ def print_solution(solution):
 def room_scheduler(rooms, classes):
     populate_room_dict(rooms)
     problem = constraint.Problem()
-    global class_dict
+    class_list = []
     for _class in classes:
         classID = get_classID(_class)
-        class_dict.update({classID : _class})
+        class_list.append(classID)
         class_rooms = []
         for room in rooms:
             temp_class = {}
@@ -90,11 +89,7 @@ def room_scheduler(rooms, classes):
             temp_class.update({'room_building' : room['room_building']})
             temp_class.update({'room_num' : room['room_num']})
             class_rooms.append(temp_class)
-        problem.addVariable(classID, class_rooms)
-
-    class_list = []
-    for key in class_dict:
-        class_list.append(str(key))
+        problem.addVariable(str(classID), class_rooms)
 
     problem.addConstraint(room_size_cons, class_list)
     problem.addConstraint(double_book_room_cons, class_list)
